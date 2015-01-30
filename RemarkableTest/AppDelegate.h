40685b677a6450d652a6604b1ef3b7e0e82d921f
Jason <jason@losoftware.co.uk>
2015-01-30 12:07:32 +0000

    Initial Commit

---
+++ RemarkableTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RemarkableTest
+//
+//  Created by Jason on 30/01/2015.
+//  Copyright (c) 2015 Entropy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RemarkableTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemarkableTest
+//
+//  Created by Jason on 30/01/2015.
+//  Copyright (c) 2015 Entropy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
